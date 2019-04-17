877dff2a9278bdea4563c7e931354cd8d12305ea
Abhishek Kumar Singh <Abhisheksingh9135@gmail.com>
2019-04-17 15:26:49 +0530

    Java Essentials Techgig

---
+++ JavaEssentials.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaEssentials {
+  public static void main(String args[]) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println(br.readLine());
+    System.out.println(br.readLine());
+    System.out.println(br.readLine());
+    System.out.println(br.readLine());
+    System.out.print(br.readLine());
+
+  }
+}
